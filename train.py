15c78d3e33d8be4449933ccaaca6bfa29235a581
svmihar <svmihar@gmail.com>
2020-09-09 04:15:58 +0700

    :fire: change data source, cause mdb is migrating

--- train.py
+++ train.py
@@ -1,15 +1,12 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 import pandas as pd
 import json
 
 df = pd.read_csv('./data/label_processed.csv')
 df.dropna(inplace=True)
-X_train,y = df['text'].values, df['label'].values
+X_train, y = df['text'].values, df['label'].values
 vectorizer = TfidfVectorizer()
 vectorizer.fit(df['text'].values)
 
@@ -21,4 +18,4 @@ yhat = [clf.predict(vectorizer.transform([a])) for a in X_train]
 
 acc = accuracy_score(yhat, y)
 with open("metrics.json", 'w') as outfile:
-        json.dump({ "accuracy": acc}, outfile)
+    json.dump({"accuracy": acc}, outfile)
