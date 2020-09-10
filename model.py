18ddaedfdf4ebd37752160b0a8ac7efd4d9b9734
svmihar <svmihar@gmail.com>
2020-09-10 20:43:35 +0700

    :fire: rest api

--- model.py
+++ model.py
@@ -1,18 +1,20 @@
 from flair.models import TextClassifier
 from flair.data import Sentence
 from pathlib import Path
+from gdown import download
 
 
 def download_model():
-    pass
+    download('https://drive.google.com/uc?id=1oiNAxhhtRV-NRMSeSj-bIXHPhNpCOjbR&export=download', output='./models/best-lm.pt')
+    return True
 
 
 def load_model(model_path='./models/'):
     mp = Path(model_path)
     if not mp.is_dir():
         mp.mkdir(exist_ok=True)
-    if not Path(mp/'best-lm.pt').is_file()
-    download_model()
+    if not Path(mp/'best-lm.pt').is_file():
+        download_model()
 
     return TextClassifier.load(mp/'best-lm.pt')
 
