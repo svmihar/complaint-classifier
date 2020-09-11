46f93d1bd0e08c4bc596effe250922531b023b5e
svmihar <svmihar@gmail.com>
2020-09-11 10:38:26 +0000

    :fire: new vanilla lstm model

---
+++ src/test_pytorch.py
@@ -0,0 +1,41 @@
+import torch
+from sklearn.metrics import accuracy_score, classification_report
+from train_pytorch import LSTM, test_iter
+
+
+def load_(load_path, model, opt):
+    state_dict = torch.load(load_path, map_location=torch.device('cuda'))
+    model.load_state_dict(state_dict["model_state_dict"])
+    opt.load_state_dict(state_dict["optimizer_state_dict"])
+    return state_dict["valid_loss"]
+
+
+def evaluate(
+    model, test_loader, version="title", threshold=0.5, device=torch.device("cuda")
+):
+    y_pred = []
+    y_true = []
+
+    model.eval()
+    with torch.no_grad():
+        for (labels, (text, text_len)), _ in test_loader:
+            labels = labels.to(device)
+            text = text.to(device)
+            text_len = text_len.to(device)
+            output = model(text, text_len)
+
+            output = (output > threshold).int()
+            y_pred.extend(output.tolist())
+            y_true.extend(labels.tolist())
+
+    print("Classification Report:")
+    print(classification_report(y_true, y_pred, labels=[1, 0], digits=4))
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda")
+    model = LSTM().to(device)
+    opt = torch.optim.AdamW(model.parameters(), lr=1e-3)
+    load_("./models/model.pt", model, opt)  # modifies the internal model, and opt
+
+    evaluate(model, test_iter, device=device)
--- train.py
+++ train.py
@@ -1,20 +1,24 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from src.model_pytorch import lstm
 import pandas as pd
 import json
 
-df = pd.read_csv('./data/label_processed.csv')
-X_train,y = df['text'].values, df['label'].values
-vectorizer = TfidfVectorizer()
-vectorizer.fit(df['text'].values)
+def model_sklearn(data_path='./data/label_processed.csv'): 
+        df = pd.read_csv(data_path)
+        X_train,y = df['text'].values, df['label'].values
+        vectorizer = TfidfVectorizer()
+        vectorizer.fit(df['text'].values)
+        X = vectorizer.transform(X_train)
+        clf = RandomForestClassifier(max_depth=100, random_state=42)
+        clf.fit(X, y)
+        yhat = [clf.predict(vectorizer.transform([a])) for a in X_train]
+        return yhat, y
 
-X = vectorizer.transform(X_train)
 
-clf = RandomForestClassifier(max_depth=100, random_state=42)
-clf.fit(X, y)
-yhat = [clf.predict(vectorizer.transform([a])) for a in X_train]
-
-acc = accuracy_score(yhat, y)
-with open("metrics.json", 'w') as outfile:
-        json.dump({ "accuracy": acc}, outfile)
+if __name__ == "__main__":
+        yhat,y = model_sklearn()
+        acc = accuracy_score(yhat, y)
+        with open("metrics.json", 'w') as outfile:
+                json.dump({ "accuracy": acc}, outfile)
