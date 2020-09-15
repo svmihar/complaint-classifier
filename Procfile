f6f21c58f538eceb27a8a094a07df90269dd0df0
svmihar <svmihar@gmail.com>
2020-09-16 01:04:26 +0700

    :sparkles: new deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bash setup.sh && uvicorn app:app --host=0.0.0.0 --port=${PORT:-5000}
--- app.py
+++ app.py
@@ -2,17 +2,20 @@ from fastapi import FastAPI
 import uvicorn
 from train import predict, load_xgboost_model
 
-m,v = load_xgboost_model()
+m, v = load_xgboost_model('./models/xgb_58.model')
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 def home():
-    return {'foo': 'bar', 'message': 'predict, and serve'}
+    return {"foo": "bar", "message": "predict, and serve"}
+
 
 @app.get("/predict/{query}")
 def pred(query: str):
-    result = predict(m,v,query.lower())
-    return {'label': result}
+    result = predict(m, v, query.lower())
+    return {"label": 'complaint' if result==1 else 'not complaint'}
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
